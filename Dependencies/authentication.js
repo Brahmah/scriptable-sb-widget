2067651bc2cde8514ea2c79f068102bdd41792ff
Bashir Rahmah <b.rahmah@ilimcollege.vic.edu.au>
2021-01-08 14:53:48 +1100

    Improve SchoolBox URL Input

--- Dependencies/authentication.js
+++ Dependencies/authentication.js
@@ -58,13 +58,19 @@ async function presentAuthMenu() {
   }
 }
 
+
 async function urlPrompt() {
-  let alert = new Alert();
-  alert.title = "Enter SchoolBox URL";
-  alert.addTextField("URL", "https://schoolbox.");
-  alert.addAction("Continue");
-  await alert.present();
-  var url = alert.textFieldValue(0);
+  let webView = new WebView();
+  // Credit To SchoolBox: The HTML Page Code Is Based Off The SchoolBox Login Form.
+  const htmlPage = `
+	<html><head><meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'><style> /*! normalize.css v3.0.3 | MIT License | github.com/necolas/normalize.css */ html { font-family: sans-serif; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100% } body { margin: 0 } [hidden] { display: none } small { font-size: 80% } input { color: inherit; font: inherit; margin: 0 } html input[type=button], input[type=reset], input[type=submit] { -webkit-appearance: button; cursor: pointer } body, html { height: 100% } *, :after, :before { -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box } body, html { font-size: 16px } body { background: #263238; color: #333; cursor: auto; font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Droid Sans", "Helvetica Neue", Helvetica, Arial, sans-serif; font-style: normal; font-weight: 400; line-height: 1.5; margin: 0; padding: 0; position: relative } div, #main, p { margin: 0; padding: 0 } p { font-family: inherit; font-size: .8125rem; font-weight: 400; line-height: 1.6; margin-bottom: .75rem; text-rendering: auto } input[type=url] { -webkit-appearance: none; -moz-appearance: none; border-radius: 0; background-color: #fff; border-style: solid; border-width: .0625rem; border-color: #c1e5ff; box-shadow: none; color: #000; display: block; font-family: inherit; font-size: .8125rem; height: 2.25rem; margin: 0 0 1rem 0; padding: .5rem; width: 100%; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box; -webkit-transition: border-color .15s linear, background .15s linear; -moz-transition: border-color .15s linear, background .15s linear; -ms-transition: border-color .15s linear, background .15s linear; -o-transition: border-color .15s linear, background .15s linear; transition: border-color .15s linear, background .15s linear } input { transition: all .15s linear; min-height: 2.5rem; line-height: inherit; font-weight: 400 } input:hover { background-color: #d1ecff; color: #000; border-color: #c1e5ff } input:focus { color: #000; background-color: #d1ecff; border-color: #74c5ff; outline: 0 } body.login { margin: 0; padding: 0; background-color: #263238; background-position: top right; background-repeat: no-repeat; background-attachment: fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover } #main { max-width: 400px; margin: auto; width: 80%; padding: 10px; margin: 0; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); } </style> <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body class="side"> <div id="main"> <div class="row collapse"> <div class="small-12 column"> <input type="url" name="url" id="url" placeholder="SchoolBox URL" value="https://schoolbox."> </div> </div> <div class="row collapse"> <div class="small-12 column"> <p style="color: white">Press "Close" In The Top Left Corner To Continue</p> </div> </div> </div></body></html>
+  `;
+  await webView.loadHTML(htmlPage);
+  await webView.present(true);
+
+  var url = await webView.evaluateJavaScript(
+    `document.getElementById("url").value`
+  );
   var isValidURL = await checkURL(url);
   if (!isValidURL) {
     Script.complete();
